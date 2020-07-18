dda0b3ca920c85f301057927bec8b3d985dc15c6
tonmoysaha <tonmoysahaopi@gmail.com>
2020-07-18 16:26:05 -0700

    initialize app

---
+++ src/test/java/com/gif/JustGifItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustGifItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
