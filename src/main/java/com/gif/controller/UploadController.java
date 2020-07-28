855b52e829c6f46ec3000b5dd01638a177724b00
tonmoysaha <tonmoysahaopi@gmail.com>
2020-07-28 16:03:04 -0700

    implement converter service

--- src/main/java/com/gif/controller/UploadController.java
+++ src/main/java/com/gif/controller/UploadController.java
@@ -3,7 +3,6 @@ package com.gif.controller;
 import java.io.File;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,18 +16,23 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadController {
 
 	private final static Logger Logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
 	@Value("${multipart.location}")
 	private String location;
 
 	@PostMapping(value = "/upload", produces = MediaType.IMAGE_GIF_VALUE)
-	public String upload(@RequestParam("file") MultipartFile multipartFile) throws IllegalStateException, IOException {
-		
-		File vedioFile = new File(location+ "/" + System.currentTimeMillis() +".mp4");
+	public String upload(@RequestParam("file") MultipartFile multipartFile, @RequestParam("start") int start,
+			@RequestParam("end") int end, @RequestParam("speed") int speed) throws IllegalStateException, IOException {
+
+		File vedioFile = new File(location + "/" + System.currentTimeMillis() + ".mp4");
 		multipartFile.transferTo(vedioFile);
-		
+
 		Logger.info("file save to{} " + vedioFile.getAbsolutePath());
 		return location;
 
 	}
 }
 //C:\Users\TONMOY~1\AppData\Local\Temp
+
+
+//my life is in hell please help me someone ....please.......i want to get out of it....please please....
---
+++ src/main/java/com/gif/service/VedioDecoderService.java
@@ -0,0 +1,5 @@
+package com.gif.service;
+
+public class VedioDecoderService {
+
+}
